57ab62008ae7411d4faeafe519c66038c33c3b5b
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-09-01 10:47:23 +0200

    add compression for `file.Content`

---
+++ file/options.go
@@ -0,0 +1,71 @@
+package file
+
+import (
+	"path/filepath"
+
+	routing "github.com/go-ozzo/ozzo-routing/v2"
+)
+
+// ServerOptions defines the possible options for the Server handler.
+type ServerOptions struct {
+	// The path that all files to be served should be located within. The path map passed to the Server method
+	// are all relative to this path. This property can be specified as an absolute file path or a path relative
+	// to the current working path. If not set, this property defaults to the current working path.
+	RootPath string
+	// The file (e.g. index.html) to be served when the current request corresponds to a directory.
+	// If not set, the handler will return a 404 HTTP error when the request corresponds to a directory.
+	// This should only be a file name without the directory part.
+	IndexFile string
+	// The file to be served when no file or directory matches the current request.
+	// If not set, the handler will return a 404 HTTP error when no file/directory matches the request.
+	// The path of this file is relative to RootPath
+	CatchAllFile string
+	// A function that checks if the requested file path is allowed. If allowed, the function
+	// may do additional work such as setting Expires HTTP header.
+	// The function should return a boolean indicating whether the file should be served or not.
+	// If false, a 404 HTTP error will be returned by the handler.
+	Allow func(*routing.Context, string) bool
+	// Define available compression encodings for serving files. Encodings are negotiated against the
+	// unser agent. The first encoding which matches the accepted encodings from the user agent as well
+	// as is available as file is served to the client.
+	Compression []Encoding
+}
+
+// Merge takes another instance of ServerOptions and merges it with the current instance.
+// Thereby other overwrites values in t if existent. The merged instance is returned as new
+// ServerOptions instance.
+func (t ServerOptions) Merge(other ServerOptions) (new ServerOptions) {
+	new = t
+
+	if other.Allow != nil {
+		new.Allow = other.Allow
+	}
+	if other.CatchAllFile != "" {
+		new.CatchAllFile = other.CatchAllFile
+	}
+	if other.Compression != nil {
+		new.Compression = other.Compression
+	}
+	if other.IndexFile != "" {
+		new.IndexFile = other.IndexFile
+	}
+	if other.RootPath != "" {
+		new.RootPath = other.RootPath
+	}
+
+	return new
+}
+
+func getServerOptions(opts []ServerOptions) ServerOptions {
+	var options ServerOptions
+
+	for _, opt := range opts {
+		options = options.Merge(opt)
+	}
+
+	if !filepath.IsAbs(options.RootPath) {
+		options.RootPath = filepath.Join(RootPath, options.RootPath)
+	}
+
+	return options
+}
--- file/server.go
+++ file/server.go
@@ -22,53 +22,6 @@ const (
 	GZip   = Encoding("gzip")
 )
 
-// ServerOptions defines the possible options for the Server handler.
-type ServerOptions struct {
-	// The path that all files to be served should be located within. The path map passed to the Server method
-	// are all relative to this path. This property can be specified as an absolute file path or a path relative
-	// to the current working path. If not set, this property defaults to the current working path.
-	RootPath string
-	// The file (e.g. index.html) to be served when the current request corresponds to a directory.
-	// If not set, the handler will return a 404 HTTP error when the request corresponds to a directory.
-	// This should only be a file name without the directory part.
-	IndexFile string
-	// The file to be served when no file or directory matches the current request.
-	// If not set, the handler will return a 404 HTTP error when no file/directory matches the request.
-	// The path of this file is relative to RootPath
-	CatchAllFile string
-	// A function that checks if the requested file path is allowed. If allowed, the function
-	// may do additional work such as setting Expires HTTP header.
-	// The function should return a boolean indicating whether the file should be served or not.
-	// If false, a 404 HTTP error will be returned by the handler.
-	Allow func(*routing.Context, string) bool
-	// Define available compression encodings for serving files. Encodings are negotiated against the
-	// unser agent. The first encoding which matches the accepted encodings from the user agent as well
-	// as is available as file is served to the client.
-	Compression []Encoding
-}
-
-func (t ServerOptions) Merge(other ServerOptions) (new ServerOptions) {
-	new = t
-
-	if other.Allow != nil {
-		new.Allow = other.Allow
-	}
-	if other.CatchAllFile != "" {
-		new.CatchAllFile = other.CatchAllFile
-	}
-	if other.Compression != nil {
-		new.Compression = other.Compression
-	}
-	if other.IndexFile != "" {
-		new.IndexFile = other.IndexFile
-	}
-	if other.RootPath != "" {
-		new.RootPath = other.RootPath
-	}
-
-	return new
-}
-
 // PathMap specifies the mapping between URL paths (keys) and file paths (keys).
 // The file paths are relative to Options.RootPath
 type PathMap map[string]string
@@ -100,13 +53,8 @@ func init() {
 //	     "/js": "/ui/dist/js",
 //	}))
 func Server(pathMap PathMap, opts ...ServerOptions) routing.Handler {
-	var options ServerOptions
-	if len(opts) > 0 {
-		options = opts[0]
-	}
-	if !filepath.IsAbs(options.RootPath) {
-		options.RootPath = filepath.Join(RootPath, options.RootPath)
-	}
+	options := getServerOptions(opts)
+
 	from, to := parsePathMap(pathMap)
 
 	// security measure: limit the files within options.RootPath
@@ -184,28 +132,20 @@ func serveFile(c *routing.Context, dir compressionDir, path string) error {
 // The file to be served can be specified as an absolute file path or a path relative to RootPath (which
 // defaults to the current working path).
 // If the specified file does not exist, the handler will pass the control to the next available handler.
-func Content(path string) routing.Handler {
-	if !filepath.IsAbs(path) {
-		path = filepath.Join(RootPath, path)
-	}
+func Content(path string, opts ...ServerOptions) routing.Handler {
+	options := getServerOptions(opts)
+
+	dir := http.Dir(options.RootPath)
+
 	return func(c *routing.Context) error {
 		if c.Request.Method != "GET" && c.Request.Method != "HEAD" {
 			return routing.NewHTTPError(http.StatusMethodNotAllowed)
 		}
-		file, err := os.Open(path)
-		if err != nil {
-			return routing.NewHTTPError(http.StatusNotFound, err.Error())
-		}
-		defer file.Close()
-		fstat, err := file.Stat()
-		if err != nil {
-			return routing.NewHTTPError(http.StatusNotFound, err.Error())
-		} else if fstat.IsDir() {
-			return routing.NewHTTPError(http.StatusNotFound)
-		}
 
-		http.ServeContent(c.Response, c.Request, path, fstat.ModTime(), file)
-		return nil
+		encodings := negotiateEncodings(c, options.Compression)
+		dir := compressionDir{dir, encodings}
+
+		return serveFile(c, dir, path)
 	}
 }
 
